fed126b8f497d20740be944deae330b93077350a
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-01-26 09:32:49 -0600

    Add some fix and dummy data

--- src/pages/bottom/Bottom.jsx
+++ src/pages/bottom/Bottom.jsx
@@ -1,352 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { api } from "../../api/api";
 import { DotTable } from "../../components/shared/dotTable/DotTable";
 import { colorScale, sortInfo, headersData } from "../../utils/";
 import "./bottom.css";
 import "./headersWidths.css";
 
 export const Bottom = () => {
-  const [data, setData] = useState([
-    {
-      ID: "1234",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: "",
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: 3,
-      Impacto_Anomalías: 2,
-      Detected_Score: 100,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "1234",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: "",
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: 3,
-      Impacto_Anomalías: 2,
-      Detected_Score: 100,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "1234",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: "",
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: 3,
-      Impacto_Anomalías: 2,
-      Detected_Score: 100,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "1234",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: "",
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: 3,
-      Impacto_Anomalías: 2,
-      Detected_Score: 100,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "1234",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: "",
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: 3,
-      Impacto_Anomalías: 2,
-      Detected_Score: 100,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "1234",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: "",
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: 3,
-      Impacto_Anomalías: 2,
-      Detected_Score: 100,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "1234",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: "",
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: 3,
-      Impacto_Anomalías: 2,
-      Detected_Score: 100,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "1234",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: "",
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: 3,
-      Impacto_Anomalías: 2,
-      Detected_Score: 100,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "1234",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: "",
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: 3,
-      Impacto_Anomalías: 2,
-      Detected_Score: 100,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "1234",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: "",
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: 3,
-      Impacto_Anomalías: 2,
-      Detected_Score: 100,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "1234",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: "",
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: 3,
-      Impacto_Anomalías: 2,
-      Detected_Score: 100,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "2345",
-      Ciudad: "Barcelona",
-      Tipologia: "Simple",
-      Comunicacion: "",
-      Pasarela_Clima: "",
-      Alumbrado: "",
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: 3,
-      Impacto_Anomalías: 2,
-      Detected_Score: 50,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "4567",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: true,
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: "",
-      Impacto_Anomalías: 2,
-      Detected_Score: 2,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "4567",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: true,
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: "",
-      Impacto_Anomalías: 2,
-      Detected_Score: 2,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "4567",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: true,
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: "",
-      Impacto_Anomalías: 2,
-      Detected_Score: 2,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "4567",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: true,
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: "",
-      Impacto_Anomalías: 2,
-      Detected_Score: 2,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "4567",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: true,
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: "",
-      Impacto_Anomalías: 2,
-      Detected_Score: 2,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "4567",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: true,
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: "",
-      Impacto_Anomalías: 2,
-      Detected_Score: 2,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "4567",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: true,
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: "",
-      Impacto_Anomalías: 2,
-      Detected_Score: 2,
-      Ahorro_Potencial: 231,
-    },
-    {
-      ID: "4567",
-      Ciudad: "Barcelona",
-      Tipologia: "Completa",
-      Comunicacion: "",
-      Pasarela_Clima: false,
-      Alumbrado: true,
-      Clima: "",
-      Banderola: "",
-      Rotulo: "",
-      Consumo_Clima: "",
-      Confort: "",
-      Anomalías: "",
-      Impacto_Anomalías: 2,
-      Detected_Score: 2,
-      Ahorro_Potencial: 231,
-    },
-  ]);
+  const [data, setData] = useState([]);
   const [newData, setNewData] = useState();
   const [incidentsArray] = useState([
     "Comunicacion",
@@ -363,6 +23,10 @@ export const Bottom = () => {
     "Total Stores",
     "%-Incidencias",
   ]);
+
+  useEffect(() => {
+    api.getDataTable().then((res) => setData(res.table));
+  }, []);
   useEffect(() => {
     data?.length !== 0 && setNewData(sortInfo(incidentsArray, data));
   }, [data, incidentsArray]);
@@ -374,37 +38,36 @@ export const Bottom = () => {
           <span>...</span>
         </div>
         <div className="headers-super-container">
-
           <div className="headers-container0">
-            <div style={{ width: '15px' }}></div>
+            <div style={{ width: "15px" }}></div>
             <div className="headers">
               {headersData.slice(0, 3).map((item) => (
-                <span className={
-                  item === 'ID'
-                    ? 'headers-widths-id'
-                    : 'headers-widths-ciudad'
-                }>{item}</span>
+                <span
+                  className={
+                    item === "ID"
+                      ? "headers-widths-id"
+                      : "headers-widths-ciudad"
+                  }
+                >
+                  {item}
+                </span>
               ))}
             </div>
           </div>
 
           <div className="headers-container">
-            <div style={{ width: '10px' }}></div>
+            <div style={{ width: "10px" }}></div>
             <div className="headers">
               {headersData.slice(3, 11).map((item) => (
-                <span className='headers-widths-dots'
-
-                >{item}</span>
+                <span className="headers-widths-dots">{item}</span>
               ))}
             </div>
           </div>
-          <div style={{ width: '10px' }}></div>
+          <div style={{ width: "10px" }}></div>
           <div className="headers-container2">
             <div className="headers">
               {headersData.slice(11, 15).map((item) => (
-                <span className='headers-widths-impacto'
-
-                >{item}</span>
+                <span className="headers-widths-impacto">{item}</span>
               ))}
             </div>
           </div>
@@ -419,56 +82,56 @@ export const Bottom = () => {
               <td className="table-data">{item.Ciudad}</td>
               <td className="table-data">{item.Tipologia}</td>
               <td className="table-data">
-                {item.Comunicacion.length !== 0 ? (
+                {item.Comunicacion?.length !== 0 ? (
                   <DotTable className={item.Pasarela_Clima ? "green" : "red"} />
                 ) : (
                   "-"
                 )}
               </td>
               <td className="table-data">
-                {item.Pasarela_Clima.length !== 0 ? (
+                {item.Pasarela_Clima?.length !== 0 ? (
                   <DotTable className={item.Pasarela_Clima ? "green" : "red"} />
                 ) : (
                   "-"
                 )}
               </td>
               <td className="table-data">
-                {item.Alumbrado.length !== 0 ? (
+                {item.Alumbrado?.length !== 0 ? (
                   <DotTable className={item.Alumbrado ? "green" : "red"} />
                 ) : (
                   "-"
                 )}
               </td>
               <td className="table-data">
-                {item.Clima.length !== 0 ? (
+                {item.Clima?.length !== 0 ? (
                   <DotTable className={item.Clima ? "green" : "red"} />
                 ) : (
                   "-"
                 )}
               </td>
               <td className="table-data">
-                {item.Banderola.length !== 0 ? (
+                {item.Banderola?.length !== 0 ? (
                   <DotTable className={item.Banderola ? "green" : "red"} />
                 ) : (
                   "-"
                 )}
               </td>
               <td className="table-data">
-                {item.Rotulo.length !== 0 ? (
+                {item.Rotulo?.length !== 0 ? (
                   <DotTable className={item.Rotulo ? "green" : "red"} />
                 ) : (
                   "-"
                 )}
               </td>
               <td className="table-data">
-                {item.Consumo_Clima.length !== 0 ? (
+                {item.Consumo_Clima?.length !== 0 ? (
                   <DotTable className={item.Consumo_Clima ? "green" : "red"} />
                 ) : (
                   "-"
                 )}
               </td>
               <td className="table-data">
-                {item.Confort.length !== 0 ? (
+                {item.Confort?.length !== 0 ? (
                   <DotTable className={item.Confort ? "green" : "red"} />
                 ) : (
                   "-"
@@ -476,10 +139,10 @@ export const Bottom = () => {
               </td>
               <td className="table-space-middle"> </td>
               <td className="table-data">
-                {item.Anomalías.length !== 0 ? item.Anomalías : "-"}
+                {item.Anomalías?.length !== 0 ? item.Anomalías : "-"}
               </td>
               <td className="table-data">
-                {item.Impacto_Anomalías.length !== 0
+                {item.Impacto_Anomalías?.length !== 0
                   ? item.Impacto_Anomalías
                   : "-"}
               </td>
@@ -489,10 +152,10 @@ export const Bottom = () => {
                   backgroundColor: colorScale(item.Detected_Score),
                 }}
               >
-                {item.Detected_Score.length !== 0 ? item.Detected_Score : "-"}
+                {item.Detected_Score?.length !== 0 ? item.Detected_Score : "-"}
               </td>
               <td className="table-data">
-                {item.Ahorro_Potencial.length !== 0
+                {item.Ahorro_Potencial?.length !== 0
                   ? item.Ahorro_Potencial
                   : "-"}
               </td>
@@ -501,13 +164,13 @@ export const Bottom = () => {
         </table>
       </div>
       <div className="table-bottom-container">
-
-
         <table className="table-bottom-style">
           <tr className="table-bottom-row">
             <td className="table-bottom-space"> </td>
             <td className="table-bottom-data-index">Total Incidencias</td>
-            <td className="table-bottom-data">{newData?.Comunicacion?.false}</td>
+            <td className="table-bottom-data">
+              {newData?.Comunicacion?.false}
+            </td>
             <td className="table-bottom-data">
               {newData?.Pasarela_Clima?.false}
             </td>
@@ -515,7 +178,9 @@ export const Bottom = () => {
             <td className="table-bottom-data">{newData?.Clima?.false}</td>
             <td className="table-bottom-data">{newData?.Banderola?.false}</td>
             <td className="table-bottom-data">{newData?.Rotulos?.false}</td>
-            <td className="table-bottom-data">{newData?.Consumo_Clima?.false}</td>
+            <td className="table-bottom-data">
+              {newData?.Consumo_Clima?.false}
+            </td>
             <td className="table-bottom-data">{newData?.Confort?.false}</td>
             <td className="table-bottom-data-space"> </td>
             <td className="table-bottom-data-space-big"> </td>
@@ -524,12 +189,16 @@ export const Bottom = () => {
             <td className="table-bottom-space"> </td>
             <td className="table-bottom-data-index">Total Stores</td>
             <td className="table-bottom-data">{newData?.Comunicacion?.true}</td>
-            <td className="table-bottom-data">{newData?.Pasarela_Clima?.true}</td>
+            <td className="table-bottom-data">
+              {newData?.Pasarela_Clima?.true}
+            </td>
             <td className="table-bottom-data">{newData?.Alumbrado?.true}</td>
             <td className="table-bottom-data">{newData?.Clima?.true}</td>
             <td className="table-bottom-data">{newData?.Banderola?.true}</td>
             <td className="table-bottom-data">{newData?.Rotulos?.true}</td>
-            <td className="table-bottom-data">{newData?.Consumo_Clima?.true}</td>
+            <td className="table-bottom-data">
+              {newData?.Consumo_Clima?.true}
+            </td>
             <td className="table-bottom-data">{newData?.Confort?.true}</td>
             <td className="table-bottom-data-space"> </td>
             <td className="table-bottom-data-space-big"> </td>
@@ -606,6 +275,6 @@ export const Bottom = () => {
           </tr>
         </table>
       </div>
-    </div >
+    </div>
   );
 };
