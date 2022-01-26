f32e2f8cab9dc70188cf8d290b00ccb14a74c6ba
César Córdoba <cordobacesar29@gmail.com>
2022-01-26 01:11:04 -0300

    dropdown and loading

--- src/components/widgetLabels/WidgetLabels.jsx
+++ src/components/widgetLabels/WidgetLabels.jsx
@@ -1,21 +1,18 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { Tag, Dots } from "@dexma/ui-components";
+import { api } from "../../api/api";
+import { DropDownTags } from "../shared/DropDownTags/DropDownTags";
 import "./widgetlabels.css";
-import { Tag } from "@dexma/ui-components";
-import { Dots } from "@dexma/ui-components";
 
 export const WidgetLabels = () => {
-  const toasts = [
-    "prueba",
-    "prueba",
-    "prueba",
-    "prueba",
-    "esta es cinco",
-    "prueba",
-    "prueba",
-    "prueba",
-    "prueba",
-    "prueba",
-  ];
+
+  const [toasts, setToast] = useState([]);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    api.getLocationTags().then(({ location_tags }) => setToast(location_tags));
+  }, []);
+
   const labels = toasts
     .slice(0, 5)
     .map((toast) => (
@@ -28,18 +25,40 @@ export const WidgetLabels = () => {
       >
         {toast}
       </Tag>
-    ));
+    )
+  );
+
+  const handleToggle = (e) => {
+    e.stopPropagation();
+    setToggle(!toggle);
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", (e) => {
+      e.target.classList.contains("tag") ||
+      e.target.classList.contains("StyledTag-kOHXTp kgNmpz")
+        ? setToggle(!toggle)
+        : setToggle(false);
+    });
+  }, [toggle]);
 
   return (
     <div className=".widget-label-container">
       <div className="widget-top">
-        {labels}
-        <div className="widget-dots">
-          <Dots steps={3} size={2} />
-        </div>
+        {labels.length > 0 ? labels : <Dots steps={3} size={6} />}
+        {labels.length >= 5 && (
+          <div className="widget-dots" onClick={handleToggle}>
+            <Dots steps={3} size={2} />
+            {toggle ? <DropDownTags tags={labels}/> : null}
+          </div>
+        )}{""}
       </div>
       <div className="widget-top">
-        <span>Tags seleccionados</span>
+        {labels.length > 0 ?
+          <span>Tags seleccionados</span>
+          :
+          <span>Tags no seleccionados</span>
+         }
       </div>
     </div>
   );
